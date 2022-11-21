8ef76e54da1db041491206f2a0cb6cd6255eb739
Sulaiman Desktop <msulaimanmisri@gmail.com>
2022-11-21 11:40:48 +0800

    Plugin Initialze

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * وَالْعَصْرِ(1)إِنَّ الْإِنْسَانَ لَفِي خُسْرٍ(2)إِلَّا الَّذِينَ ءَامَنُوا وَعَمِلُوا الصَّالِحَاتِ وَتَوَاصَوْا بِالْحَقِّ وَتَوَاصَوْا بِالصَّبْرِ
+ * Demi masa. Sesungguhnya manusia itu benar-benar berada dalam kerugian, kecuali orang-orang
+ * yang beriman dan mengerjakan amal saleh dan nasehat menasehati supaya mentaati kebenaran
+ * dan nasehat menasehati supaya menetapi kesabaran”.
+ * (al-‘Asr: 1-3)
+ */
---
+++ sm-scrollbar.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:       Ahlan API
+ * Plugin URI:        https://github.com/Asia-Quest-Malaysia
+ * Description:       This plugin will create custom API Endpoint to allow AQM Developers to GET / POST / PATCH the data from custom Application
+ * Version:           1.4
+ * Author:            Sulaiman Misri
+ * Author URI:        https://github.com/msulaimanmisri
+ * Text Domain:       ahlan-api
+ * Domain Path:       /languages
+ */
